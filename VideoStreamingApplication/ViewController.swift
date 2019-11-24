b7003daa7ff2d1dff6579a1ade0a795437306965
dev-shanghai <iamasimkhanjadoon@gmail.com>
2019-11-24 22:32:45 +0400

    Initial Commit

---
+++ VideoStreamingApplication/ViewController.swift
@@ -0,0 +1,29 @@
+///**
+/**
+VideoStreamingApplication
+Created by: dev shanghai on 24/11/2019
+
+(** ViewController.swift **)
+dev_shanghai
+Copyright © 2019 dev_shanghai. All rights reserved.
+
++-----------------------------------------------------+
+|                                                     |
+|                                                     |
+|                                                     |
++-----------------------------------------------------+
+
+*/
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ VideoStreamingApplicationTests/VideoStreamingApplicationTests.swift
@@ -0,0 +1,43 @@
+///**
+/**
+VideoStreamingApplicationTests
+Created by: dev shanghai on 24/11/2019
+
+(** VideoStreamingApplicationTests.swift **)
+dev_shanghai
+Copyright © 2019 dev_shanghai. All rights reserved.
+
++-----------------------------------------------------+
+|                                                     |
+|                                                     |
+|                                                     |
++-----------------------------------------------------+
+
+*/
+
+import XCTest
+@testable import VideoStreamingApplication
+
+class VideoStreamingApplicationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
